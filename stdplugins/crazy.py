09fdf85d2ea62f886940165f7e82bf32ca22ed82
sonu2003 <57164715+sonu2003@users.noreply.github.com>
2019-12-06 04:40:48 +0530

    Create crazy.py

---
+++ stdplugins/crazy.py
@@ -0,0 +1,19 @@
+# (c) @UniBorg
+# Original edit by @AyushChatterjee
+""".sun Plugin for @BotHub"""
+
+
+from telethon import events
+import asyncio
+from collections import deque
+
+
+@borg.on(events.NewMessage(pattern=r"\.sun", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("😂😭❤️😡😂😭❤️😡"))
+	for _ in range(60):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
