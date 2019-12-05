e4c7c6a977a285b08649bf56cbd8c31438205b8d
sonu2003 <57164715+sonu2003@users.noreply.github.com>
2019-12-06 04:15:03 +0530

    Update hack.py

--- stdplugins/hack.py
+++ stdplugins/hack.py
@@ -38,7 +38,7 @@ async def _(event):
             "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
             "`Hacking... 100%\n█████████HACKED███████████ `",
-            "`Targeted Account Hacked...\n\nPay 9999$ To @Three_Cube_TeKnoways or a Plate of Samosas To Remove This Hack`"
+            "`Targeted Account Hacked...\n\nPay 9999$ To @AyushChatterjee or a Plate of Samosas To Remove This Hack`"
         ]
 
         for i in animation_ttl:
