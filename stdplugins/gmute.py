2b68ca0155ae65fb990df5846333505d93f5af85
sonu2003 <57164715+sonu2003@users.noreply.github.com>
2019-12-11 20:54:49 +0530

    Update gmute.py

--- stdplugins/gmute.py
+++ stdplugins/gmute.py
@@ -1,117 +1,93 @@
 """
 cmds: .gmute user_id|Reply to user messsage.	
       .ungmute user_id|Reply to user messsage. 
-      .listgmuted|List Currently G-Muted Users.
 
 By:- @AyushChatterjee
 """
 
-from telethon import events
-import logging
-import asyncio
-from uniborg.util import admin_cmd
-logging.basicConfig(level=logging.INFO)
-MONGO_URI = Config.MONGO_URI
-try:	
-	db = mongo_client['test']
-	db = db.muted
 
-except Exception as e:
-	logging.error(str(e))	
+@borg.on(outgoing=True, pattern="^.ungmute(?: |$)(.*)", groups_only=True)
+async def ungmoot(un_gmute):
+    """ For .ungmute command, ungmutes the target in the userbot """
+    # Admin or creator check
+    chat = await un_gmute.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
 
-@borg.on(admin_cmd(pattern="gmute ?(.*)", allow_sudo=True))
-async def gmute_user(event):
-	if event.fwd_from:
-		return
-	input_str = event.pattern_match.group(1)	
-	if not event.reply_to_msg_id and not input_str:
-		await event.edit("`Give a User id or Reply To a User Message To Mute.`")
-		return	
-	if event.reply_to_msg_id:
-		msg = await event.get_reply_message()
-		user_id = msg.from_id	
-	else:
-		user_id = int(input_str)
+    # If not admin and not creator, return
+    if not admin and not creator:
+        await un_gmute.edit(NO_ADMIN)
+        return
 
-	await event.edit("`Getting a duct tape..`")	
-	try:
-		chat = await event.get_chat()
-		is_admin = chat.admin_rights
-		is_creator = chat.creator
-	except:
-		await event.edit("`You Need to Run this command in a Group Chat.`")	
-		return
-	if is_admin or is_creator:	
-		try:
-			c = muted.find({})
-			for i in c:
-				if i['user_id'] == user_id:
-					await event.edit("`User is Already G-Muted.`")
-					return
-			if user_id == borg.me.id:
-					await event.edit("`Cant Mute Myself..`")
-					return
-			else:
-				muted.insert_one({'user_id':user_id})
-				await event.edit("`G-Muted` [{}](tg://user?id={}).".format(str(user_id),str(user_id)))
-				logging.info("G-Muted {}".format(str(user_id)))
-		except Exception as e:
-			logging.error(str(e))
-			await event.edit("Error: "+str(e))
-			return
-	else:		
-		await event.edit("`You are not admin Here.`")	
+    # Check if the function running under SQL mode
+    try:
+        from userbot.modules.sql_helper.gmute_sql import ungmute
+    except AttributeError:
+        await un_gmute.edit(NO_SQL)
+        return
 
-@borg.on(admin_cmd(pattern="ungmute ?(.*)", allow_sudo=True))
-async def un_gmute_user(event):
-	if event.fwd_from:
-		return
-	input_str = event.pattern_match.group(1)	
-	if not event.reply_to_msg_id and not input_str:
-		await event.edit("`Give a User id or Reply To a User Message To Mute.`")
-		return	
-	if event.reply_to_msg_id:
-		msg = await event.get_reply_message()
-		user_id = msg.from_id	
-	else:
-		user_id = int(input_str)	
-	await event.edit("`Removing Duct Tape from User's Mouth.`")	
-	try:
-		muted.delete_one({'user_id':user_id})
-		await event.edit("`Un-Gmuted` [{}](tg://user?id={}).".format(str(user_id),str(user_id)))
-		logging.info("Un-Gmuted {}".format(str(user_id)))
-	except Exception as e:
-		logging.error(str(e))
-		await event.edit("Error: "+str(e))
+    user = await get_user_from_event(un_gmute)
+    user = user[0]
+    if user:
+        pass
+    else:
+        return
 
-@borg.on(admin_cmd(pattern="listgmuted", allow_sudo=True))
-async def list_gmuted(event):
-	if event.fwd_from:
-		return
-	try:
-		cur = muted.find({})
-		msg = "**G-Muted Users:**\n"
-		for c in cur:
-			msg += "__User:__ `"+str(c['user_id'])+"`\n"
-		await event.edit(msg)
-	except Exception as e:
-		logging.error(str(e))
-		await event.edit("Error: "+str(e))	
+    # If pass, inform and start ungmuting
+    await un_gmute.edit('```Ungmuting...```')
 
-@borg.on(events.NewMessage())      
-async def gmute_listener(sender):			
-	if MONGO_URI is None:
-		return
-	try:
-		curs = muted.find({})
-		for c in curs:
-			if c['user_id'] == sender.from_id:
-				await sender.delete()
-	except:
-		return 
-			
+    if ungmute(user.id) is False:
+        await un_gmute.edit("`Error! User probably not gmuted.`")
+    else:
+        # Inform about success
+        await un_gmute.edit("```Ungmuted Successfully```")
+
+        if BOTLOG:
+            await un_gmute.client.send_message(
+                BOTLOG_CHATID, "#UNGMUTE\n"
+                f"USER: [{user.first_name}](tg://user?id={user.id})\n"
+                f"CHAT: {un_gmute.chat.title}(`{un_gmute.chat_id}`)")
 
 
+@borg.on(outgoing=True, pattern="^.gmute(?: |$)(.*)", groups_only=True)
+async def gspider(gspdr):
+    """ For .gmute command, globally mutes the replied/tagged person """
+    # Admin or creator check
+    chat = await gspdr.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
 
+    # If not admin and not creator, return
+    if not admin and not creator:
+        await gspdr.edit(NO_ADMIN)
+        return
 
+    # Check if the function running under SQL mode
+    try:
+        from userbot.modules.sql_helper.gmute_sql import gmute
+    except AttributeError:
+        await gspdr.edit(NO_SQL)
+        return
 
+    user, reason = await get_user_from_event(gspdr)
+    if user:
+        pass
+    else:
+        return
+
+    # If pass, inform and start gmuting
+    await gspdr.edit("`Grabs a huge, sticky duct tape!`")
+    if gmute(user.id) is False:
+        await gspdr.edit(
+            '`Error! User probably already gmuted.\nRe-rolls the tape.`')
+    else:
+        if reason:
+            await gspdr.edit(f"`Globally taped!`Reason: {reason}")
+        else:
+            await gspdr.edit("`Globally taped!`")
+
+        if BOTLOG:
+            await gspdr.client.send_message(
+                BOTLOG_CHATID, "#GMUTE\n"
+                f"USER: [{user.first_name}](tg://user?id={user.id})\n"
+                f"CHAT: {gspdr.chat.title}(`{gspdr.chat_id}`)")
