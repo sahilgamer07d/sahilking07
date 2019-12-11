29b7ad6bf690ba029729af5ee945bce71d843253
sonu2003 <57164715+sonu2003@users.noreply.github.com>
2019-12-11 19:08:23 +0530

    Update afk.py

--- stdplugins/afk.py
+++ stdplugins/afk.py
@@ -122,7 +122,7 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"My Master **3Cube** Is **AFK since**: {afk_time} " + \
+        message_to_reply = f"My Master **AYUSH** Is **AFK since**: {afk_time} " + \
             f"\n\n__I promise HE'll be back within a few hours__\n\n**Reason for his AFK ~** {reason}" \
             if reason \
             else f"My Master **AYUSH** is **AFK Since** {afk_since} so wait until He is back.\n\n**THANKS**"
