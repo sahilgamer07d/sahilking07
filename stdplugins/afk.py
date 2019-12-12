e237b8ac7e274ab39327a4a7cc59c4cc345d0809
sonu2003 <57164715+sonu2003@users.noreply.github.com>
2019-12-12 16:53:53 +0530

    Update afk.py

--- stdplugins/afk.py
+++ stdplugins/afk.py
@@ -122,10 +122,10 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"MY MASTER **AYUSH** IS **AFK** SINCE : {afk_since} " + \
+        message_to_reply = f"MY MASTER **AYUSH** IS **AFK** SINCE : {afk_time} " + \
             f"\n\n__I HAVE INFORMED HIM OF YOUR MESSAGE !__\n\n**REASON FOR HIS AFK : ** {reason}" \
             if reason \
-            else f"MY MASTER **AYUSH** IS **AFK** SINCE : {afk_since} ; PLEASE WAIT FOR HIS RETURN.\n\n**THANKS**"
+            else f"MY MASTER **AYUSH** IS **AFK** SINCE : {afk_time} ; PLEASE WAIT FOR HIS RETURN.\n\n**THANKS**"
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in last_afk_message:  # pylint:disable=E0602
